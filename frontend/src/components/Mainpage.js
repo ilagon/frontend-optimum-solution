92f3e0ec460bb5a904e6862f4ec298c661ba3b1c
Dzul Ariffin <idzul95@gmail.com>
2020-06-12 14:50:30 +0800

    Used Material UI, for Sidebar, do note that I've commented out the previous version, Dzul, 12 june

--- frontend/src/components/Mainpage.js
+++ frontend/src/components/Mainpage.js
@@ -3,15 +3,14 @@ import CustomerDetails from './CustomerDetails'
 import ApprovalStatus from './ApprovalStatus'
 import CreditCardStatus from './CreditCardStatus'
 import Overview from './Overview'
-import Sidebar from './sidebar/Sidebar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import Header from './header/header'
 
 export default function Mainpage() {
     return (
         <div>
             <Router>
-                <Sidebar></Sidebar>
+                <Header />
                 <Switch>
                     <Route exact path='/'>
                         <Overview></Overview>
