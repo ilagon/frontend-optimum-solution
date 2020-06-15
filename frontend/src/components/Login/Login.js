1a5f3c187ffa13ff0d5f1de7247152d95077a86b
Hannah Kwah <hannahkwah@Hannahs-iMac.local>
2020-06-15 12:51:03 +0800

    Co-authored-by: nhoiyee <nhoiyee@yahoo.com.sg>

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -36,10 +34,8 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const getUsers = () => {
         axios.get("http://localhost:7001/users/",
-            // {
-            //   headers: {
-            //     authorization: "Bearer " + token
-            //   }}
+            email,
+            password
         )
             .then(res => console.log(res))
             .catch(err => { console.log(err) })
@@ -98,10 +94,12 @@ const Login = () => {
                             fullWidth
                             variant="contained"
                             className={classes.submit}
+                            onClick={getUsers}
                         >
                             Login
                         </Button>
 
+                        <Typography><a href="/ForgetPass">Forget Password?</a></Typography>
                     </form>
                 </div>
 
