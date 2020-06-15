2cea1e57b9a25eb742b172866855d8438a09495d
Dzul Ariffin <idzul95@gmail.com>
2020-06-15 21:26:44 +0800

    Fixed some colour schemes, partially working search function on 'Customer Details', Dzul, 15 June

--- frontend/src/components/admin_components/header/header.js
+++ frontend/src/components/admin_components/header/header.js
@@ -1,39 +1,44 @@
-import React from 'react'
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Drawer from '@material-ui/core/Drawer';
-import Sidebar from './../sidebar/Sidebar'
+import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Drawer from "@material-ui/core/Drawer";
+import Sidebar from "./../sidebar/Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+
+  palette: {
+    contrastText: "#fff",
+  },
 }));
 
 export default function Header() {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}></Typography>
-                    <AccountCircleIcon />
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent">
-                <Sidebar></Sidebar>
-            </Drawer>
-        </div>
-    )
+  return (
+    <div>
+      <AppBar position="static" className={classes.root}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <FontAwesomeIcon icon={faUserCircle} size="2x" color="#0e4686" />
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent">
+        <Sidebar></Sidebar>
+      </Drawer>
+    </div>
+  );
 }
