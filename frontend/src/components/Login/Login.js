13df8bf46082be0a732c7cb731909bad672c50aa
Hannah Kwah <hannahkwah@Hannahs-iMac.local>
2020-06-15 11:04:04 +0800

    Login edits

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,53 +1,18 @@
 import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from 'axios'
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Circuit Breakers BankApp Phase 2
-      </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
 const useStyles = makeStyles((theme) => ({
     root: {
         height: '100vh',
-    },
-    image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    },
-    paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
     },
     form: {
         width: '100%', // Fix IE 11 issue.
@@ -55,6 +20,11 @@ const useStyles = makeStyles((theme) => ({
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+        backgroundColor: '#AA3A21',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: '#AA3A21'
+          },
     },
 }));
 
@@ -86,28 +56,23 @@ const Login = () => {
 
     return (
         <div id="container">
-            <img src="https://source.unsplash.com/random" id="loginpic" />
+            <img src="https://source.unsplash.com/ULwzqOnPem0" id="loginpic" />
             <Container id="login" component="main" maxWidth="xs">
                 <CssBaseline />
                 <Grid id="Container" item xs={false} sm={4} md={7} className={classes.image} />
 
-
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
+                <div>
                     <Typography component="h1" variant="h5">
                         LOGIN
                     </Typography>
                     <Typography component="h1" variant="h5">
-                        Register
+                        <a href="/SignUp">REGISTER</a>
                     </Typography>
                     <form className={classes.form} noValidate>
                         <Grid container spacing={2}>
 
                             <Grid item xs={12}>
                                 <TextField
-                                    variant="outlined"
                                     required
                                     fullWidth
                                     id="email"
@@ -118,7 +83,6 @@ const Login = () => {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
-                                    variant="outlined"
                                     required
                                     fullWidth
                                     name="password"
@@ -128,29 +92,18 @@ const Login = () => {
                                     onChange={handlePassword}
                                 />
                             </Grid>
-
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="Change this to the error message"
-                                />
-                            </Grid>
                         </Grid>
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
-                            color="primary"
                             className={classes.submit}
                         >
                             Login
-          </Button>
+                        </Button>
 
                     </form>
                 </div>
-                <Box mt={5}>
-                    <Copyright />
-                </Box>
 
             </Container>
 
