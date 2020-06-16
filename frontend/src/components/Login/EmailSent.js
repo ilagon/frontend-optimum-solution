0c2e3e30785acf8f99432c4dbc6348ca2a52e9fc
Hannah Kwah <hannahkwah@Hannahs-iMac.local>
2020-06-16 23:34:53 +0800

    revised email sent and logout

--- frontend/src/components/Login/EmailSent.js
+++ frontend/src/components/Login/EmailSent.js
@@ -1,18 +1,70 @@
 import React from 'react';
-
-import "./EmailSent.css";
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-function EmailSent() {
-    return(
-        <div className="EmailSent">
-            <nav id= "email-icons" />
-                <MailOutlineIcon />
-            <h1>Email Sent!</h1>
-            <p>We've sent you an email with a link to reset your password</p>
-            <p>Didn't get the email or not your email address? <a href=" ">Try again.</a></p>
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    paper: {
+        marginTop: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    title: {
+        color: '#173A77',
+        fontFamily: [
+            'Avenir Heavy',
+            'sans serif',
+        ]
+    },
+    bodytxt: {
+        color: '#173A77',
+        fontFamily: [
+            'Avenir Medium',
+            'sans serif',
+        ]
+    },
+    bodytxt2: {
+        color: '#AA3A21',
+        fontFamily: [
+            'Avenir Medium',
+            'sans serif',
+        ]
+    }
+}));
+
+
+
+export default function EmailSent() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Container id="logout" component="main" maxWidth="sm">
+                
+                <div className={classes.paper}>
+                    <MailOutlineIcon style={{ color: "AA3A21", fontSize: 120 }} />
+                    <Typography component="h1" variant="h2" className={classes.title}>
+                        Email Sent!
+                    </Typography>
+                    <Typography component="h2" variant="h6" className={classes.bodytxt}>
+                        We've sent you an email with a link to reset your password
+                    </Typography>
+                    <Typography component="h2" variant="h6" className={classes.bodytxt}>
+                        Didn't get the email or not your email address? <Link href="/ForgetPass" className={classes.bodytxt2}>Try again.</Link>
+
+                    </Typography>
+                    
+                </div>
+            </Container>
+
         </div>
-    );
+    )
 }
-
-export default EmailSent;
--- frontend/src/components/Login/Logout.js
+++ frontend/src/components/Login/Logout.js
@@ -1,80 +1,136 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-import "./logout.css";
-
-class Logout extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { logginStatus: true };
-    this.events = [
-      "load",
-      "mousemove",
-      "mousedown",
-      "click",
-      "scroll",
-      "keypress"
-    ];
-
-    this.warn = this.warn.bind(this);
-    this.logout = this.logout.bind(this);
-    this.resetTimeout = this.resetTimeout.bind(this);
-
-    for (var i in this.events) {
-      window.addEventListener(this.events[i], this.resetTimeout);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    paper: {
+        marginTop: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    title: {
+        color: '#173A77',
+        fontFamily: [
+            'Avenir Heavy',
+            'sans serif',
+        ]
+    },
+    bodytxt: {
+        color: '#173A77',
+        fontFamily: [
+            'Avenir Medium',
+            'sans serif',
+        ]
     }
+}));
 
-    this.setTimeout();
-  }
+export default function Logout() {
+    const classes = useStyles();
 
-  clearTimeout() {
-    if (this.warnTimeout) clearTimeout(this.warnTimeout);
-
-    if (this.logoutTimeout) clearTimeout(this.logoutTimeout);
-  }
-
-  setTimeout() {
-    this.warnTimeout = setTimeout(this.warn, 16 * 1000);
-
-    this.logoutTimeout = setTimeout(this.logout, 30 * 1000);
-  }
-
-  resetTimeout() {
-    this.clearTimeout();
-    this.setTimeout();
-  }
-
-  warn() {
-    alert("You will be logged out automatically in 1 minute.");
-  }
-
-  logout() {
-    // Send a logout request to the API
-    console.log("Sending a logout request to the API...");
-    this.setState({ logginStatus: false });
-    // this.destroy(); // Cleanup
-  }
-
-  destroy() {
-    this.clearTimeout();
-
-    for (var i in this.events) {
-      window.removeEventListener(this.events[i], this.resetTimeout);
-    }
-  }
-  render() {
     return (
-      <div className="App">
-        <h1>Logout</h1>
-        <h2>Thank you for banking with us!</h2>
-        {/* <h2>Status :{this.state.logginStatus ? "Loggin" : "Logout"}</h2> */}
-      </div>
-    );
-  }
+        <div className={classes.root}>
+            <Container id="logout" component="main" maxWidth="sm">
+
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h2" className={classes.title}>
+                        Logout
+                    </Typography>
+                    <Typography component="h2" variant="h6" className={classes.bodytxt}>
+                        Thank you for banking with us!
+                    </Typography>
+                    <Typography component="h2" variant="h6">
+                        <Link href="/Login" className={classes.bodytxt}>Back to Login</Link>
+                    </Typography>
+                </div>
+
+            </Container>
+
+        </div>
+    )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Logout />, rootElement);
+// class Logout extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-export default Logout;
+//     this.state = { logginStatus: true };
+//     this.events = [
+//       "load",
+//       "mousemove",
+//       "mousedown",
+//       "click",
+//       "scroll",
+//       "keypress"
+//     ];
+
+//     this.warn = this.warn.bind(this);
+//     this.logout = this.logout.bind(this);
+//     this.resetTimeout = this.resetTimeout.bind(this);
+
+//     for (var i in this.events) {
+//       window.addEventListener(this.events[i], this.resetTimeout);
+//     }
+
+//     this.setTimeout();
+//   }
+
+//   clearTimeout() {
+//     if (this.warnTimeout) clearTimeout(this.warnTimeout);
+
+//     if (this.logoutTimeout) clearTimeout(this.logoutTimeout);
+//   }
+
+//   setTimeout() {
+//     this.warnTimeout = setTimeout(this.warn, 16 * 1000);
+
+//     this.logoutTimeout = setTimeout(this.logout, 30 * 1000);
+//   }
+
+//   resetTimeout() {
+//     this.clearTimeout();
+//     this.setTimeout();
+//   }
+
+//   warn() {
+//     alert("You will be logged out automatically in 1 minute.");
+//   }
+
+//   logout() {
+//     // Send a logout request to the API
+//     console.log("Sending a logout request to the API...");
+//     this.setState({ logginStatus: false });
+//     // this.destroy(); // Cleanup
+//   }
+
+//   destroy() {
+//     this.clearTimeout();
+
+//     for (var i in this.events) {
+//       window.removeEventListener(this.events[i], this.resetTimeout);
+//     }
+//   }
+//   render() {
+//     return (
+//       <div className="Logout">
+//         <h1>Logout</h1>
+//         <h2>Thank you for banking with us!</h2>
+//         {/* <h2>Status :{this.state.logginStatus ? "Loggin" : "Logout"}</h2> */}
+//       </div>
+//     );
+//   }
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Logout />, rootElement);
+
+// export default Logout;
+
+
