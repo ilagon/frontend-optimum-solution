6c556251be132b45926a8de792883f6ac9a4e9a4
Dzul Ariffin <idzul95@gmail.com>
2020-06-16 14:52:38 +0800

    Fixed stuff

--- frontend/src/components/admin_components/Mainpage.js
+++ frontend/src/components/admin_components/Mainpage.js
@@ -4,13 +4,15 @@ import ApprovalStatus from "./ApprovalStatus";
 import CreditCardStatus from "./CreditCardStatus";
 import Overview from "./Overview";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./header/header";
+import Header from "./header/Header";
+import Sidebar from "./sidebar/Sidebar";
 
 export default function Mainpage() {
   return (
     <div>
+      <Header />
       <Router>
-        <Header />
+        <Sidebar />
         <Switch>
           <Route exact path="/">
             <Overview></Overview>
