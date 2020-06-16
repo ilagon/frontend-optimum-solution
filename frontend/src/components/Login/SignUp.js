b2a568035bfc13deecaf4d73a5971b4687b8f2fd
Sharxn13 <sharxnrxse1312@gmail.com>
2020-06-16 21:13:59 +0800

    Sharon 16-06-2020 Login & Register Pages somewhat done

--- frontend/src/components/Login/SignUp.js
+++ frontend/src/components/Login/SignUp.js
@@ -4,6 +4,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,20 +14,52 @@ import Container from '@material-ui/core/Container';
 import axios from 'axios'
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
+    root: {
+        height: '100vh',
+      },
+      image: {
+        backgroundImage: 'url(https://source.unsplash.com/ULwzqOnPem0)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'absolute',
+      },
+      links: {
+        textDecoration: 'none',
+        fontFamily: 'Helvetica Neue',
+        color: 'black',
+        fontWeight: '700',
+        fontSize: '1.5rem'
+      },
+      paper: {
+        margin: theme.spacing(8, 4),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
+      },
+      title: {
+        fontFamily: 'Arial',
+        color: '#ab3a22',
+        fontSize: '2.5rem',
+        fontWeight: '700',
+        fontStyle: 'italic'
+      },
+      form: {
+        width: '25rem', // Fix IE 11 issue.
+        position: 'relative',
+        marginTop: theme.spacing(1),
+        color: 'black'
+      },
+      submit: {
         margin: theme.spacing(3, 0, 2),
-    },
-}));
+        backgroundColor: '#ab3a22',
+        width: '25rem',
+        height: '3rem'
+      },
+      forgetButton: {
+        color: 'black',
+        fontStyle: 'bold'
+      }
+    }));
 
 
 export default function SignUp() {
@@ -74,92 +109,76 @@ export default function SignUp() {
 
     }
 
-
-
-
     return (
-        <div id="container">
-            <img src="https://source.unsplash.com/ULwzqOnPem0" id="loginpic" />
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div className={classes.paper}>
-                    <Typography component="h1" variant="h5">
-                    <a href="/Login">LOGIN</a>
-                    </Typography>
-                    <Typography component="h1" variant="h5">
-                        REGISTER
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} >
-                                <TextField
-                                    autoComplete="fullname"
-                                    name="FullName"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="FullName"
-                                    label="Full Name"
-                                    autoFocus
-                                    onChange={handleName}
-                                />
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    onChange={handleEmail}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    onChange={handlePassword}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="confrimPass"
-                                    label="confrim Password"
-                                    type="password"
-                                    id="password"
-                                    onChange={handlePassword1}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="Change this to the error message"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                            onClick={registerUser} >
-                            Sign Up
-                        </Button>
-                        
-                    </form>
-                </div>
-            </Container>
-        </div>
-    );
-}
+        <Grid container component="main" className={classes.root}>
+          <CssBaseline />
+          <Grid item xs={false} sm={4} md={7} className={classes.image} />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <div className={classes.paper} >
+            <h1 className={classes.title}>Optimum DigiBank</h1> 
+            <Box display="flex" p={1} bgcolor="background.paper">
+              <Box p={5}>
+                <a href="/Login" className={classes.links}>LOGIN</a>
+              </Box>
+              <Box p={5}>
+                <a href="/SignUp" className={classes.links}>REGISTER</a>
+              </Box>
+            </Box>
+              <form className={classes.form} noValidate>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  floatingLabel={true}
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="email"
+                  label="Email Address"
+                  type="email"
+                  id="email"
+                  autoComplete="email"
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="retype"
+                  label="Retype Password"
+                  type="retype"
+                  id="retype"
+                  autoComplete="current-password"
+                />
+                <div align="center">
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Sign Up
+                </Button></div>
+              </form>
+            </div>
+          </Grid>
+        </Grid>
+      );
+    }
