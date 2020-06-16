8e4185f85f4bad968d7f6e73e851fb719ecabc2a
Kenny <kenny.thio@gmail.com>
2020-06-16 18:26:48 +0800

    Kenny 16/6/2020 6:26PM updated backend

--- backend/routes/creditcard.js
+++ backend/routes/creditcard.js
@@ -18,21 +18,21 @@ router.get("/search/approve", creditcardController.creditcard_search_by_cardStat
 //overview customer list search by card status reject
 router.get("/search/reject", creditcardController.creditcard_search_by_cardStatus_reject);
 
-router.get("/search/:id",creditcardController.creditcard_search_by_userid);
-
-//overview customer list search by card id
-router.get("/:cardId", creditcardController.creditcard_search_by_cardid);
-
-//credit card create
-router.post("/create",creditcardController.creditcard_create);
 
 //credit card approval approve function
 router.patch("/approve/:cardId", creditcardController.creditcard_search_by_cardStatus_approve);
+
+//credit card create
+router.post("/create",creditcardController.creditcard_create);
 
 //credit card approval reject function
 router.patch("/reject/:cardId", creditcardController.creditcard_search_by_cardStatus_reject);
 
 //reset credit balance to credit limit
 router.patch("/resetBalance", creditcardController.reset_credit_balance);
+
+//overview customer list search by card id
+router.get("/:cardId", creditcardController.creditcard_search_by_cardid);
+
 
 module.exports = router;
