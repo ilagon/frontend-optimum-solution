d09e90c1488e2053b06bc50704fea46fc2d29117
Chen Jingguo <chenjingguo97@gmail.com>
2020-06-17 16:29:35 +0800

    index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import reducer from "./components/common/redux/rootReducer";
+import { Provider } from "react-redux";
+
+export const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
