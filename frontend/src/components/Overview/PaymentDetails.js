01e358a85720e1fa9385f7343cd3564cf4a02b4e
Zafy <zafirahrosley96@gmail.com>
2020-06-19 15:51:26 +0800

    overview page front end functions - zafy

--- frontend/src/components/Overview/PaymentDetails.js
+++ frontend/src/components/Overview/PaymentDetails.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,54 +11,25 @@ import TablePagination from '@material-ui/core/TablePagination';
 import './Overview.css';
 
 const columns = [
-  { id: 'paymentId', label: 'Payment Id'},
-  { id: 'paymentType', label: 'Payment Type'},
+  { id: 'transfer_number', label: 'Payment Id'},
+  { id: 'payment_type', label: 'Payment Type'},
   {
-    id: 'paymentAmount',
+    id: 'payment_amount',
     label: 'Payment Amount',
     format: (value) => "$" + value.toFixed(2)
   },
   {
-    id: 'dateTime',
+    id: 'date_time',
     label: 'Date & Time',
   },
   {
-    id: 'customerID',
+    id: 'customer_id',
     label: 'Customer ID',
   },
   {
-    id: 'transferId',
+    id: 'transfer_number',
     label: 'Transfer ID',
   },
-];
-
-// Generate Payment History Data
-function createData(paymentId, paymentType, paymentAmount, dateTime, customerID, transferId) {
-  return { paymentId, paymentType, paymentAmount, dateTime, customerID, transferId };
-}
-
-/* Data Example for Table Pagination */
-const rows = [
-  createData(1, 'Phone Bill', 20, '05-05-2020 13:43', 12345, 1),
-  createData(2, 'Tax', 100, '06-05-2020 14:43', 13456, 2),
-  createData(3, 'Transfer Money', 500, '07-05-2020 15:43', 14567, 3),
-  createData(4, 'Tax', 300, '08-05-2020 16:43', 88888, 4),  
-  createData(5, 'Phone Bill', 20, '05-05-2020 13:43', 12345, 5),
-  createData(6, 'Tax', 100, '06-05-2020 14:43', 13456, 6),
-  createData(7, 'Transfer Money', 500, '07-05-2020 15:43', 14567, 7),
-  createData(8, 'Tax', 300, '08-05-2020 16:43', 88888, 8),
-  createData(9, 'Transfer Money', 500, '07-05-2020 15:43', 14567, 9),
-  createData(10, 'Tax', 300, '08-05-2020 16:43', 88888, 10),  
-  createData(11, 'Phone Bill', 20, '05-05-2020 13:43', 12345, 11),
-  createData(12, 'Tax', 100, '06-05-2020 14:43', 13456, 12),
-  createData(13, 'Transfer Money', 500, '07-05-2020 15:43', 14567, 13),
-  createData(14, 'Tax', 300, '08-05-2020 16:43', 88888, 14),  
-  createData(15, 'Phone Bill', 20, '05-05-2020 13:43', 12345, 15),
-  createData(16, 'Tax', 100, '06-05-2020 14:43', 13456, 16),
-  createData(17, 'Transfer Money', 500, '07-05-2020 15:43', 14567, 17),
-  createData(18, 'Tax', 300, '08-05-2020 16:43', 88888, 18),
-  createData(19, 'Transfer Money', 500, '07-05-2020 15:43', 14567,19),
-  createData(20, 'Tax', 300, '08-05-2020 16:43', 88888, 20),
 ];
 
 const useStyles = makeStyles({
@@ -69,10 +41,20 @@ const useStyles = makeStyles({
   },
 });
 
-export default function PaymentDetails() {
+export default function PaymentDetails(props) {
+  const [rows, setRows] = useState([]);
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const creditCardId = props.creditCardId;
+  const customerId = props.customerId;
+
+    axios.get("http://localhost:9002/payment_history/" + creditCardId)
+    .then(res => {
+      setRows([...res.data.Users]);
+    })
+    .catch(error => console.log(error))
+  console.log(rows);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -111,7 +93,10 @@ export default function PaymentDetails() {
                     const value = row[column.id];
                     return (
                       <TableCell key={column.id} align={column.align} className="paymentTableCell">
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                        {
+                          column.id === 'customer_id' ? customerId :
+                          column.format && typeof value === 'number' ? column.format(value) : value
+                        }
                       </TableCell>
                     );
                   })}
