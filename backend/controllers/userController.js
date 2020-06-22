c1c4320008fbc3c188d1bf20f732eb4ecd81d4fd
Ain <RoquetteAce@users.noreply.github.com>
2020-06-22 11:59:37 +0800

    Ain deleted customer change user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,155 +1,159 @@
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
 const mailgun = require("mailgun-js");
-const jwt = require("jsonwebtoken")
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const mg = mailgun({
-    apiKey: process.env.MAILGUN_APIKEY,
-    domain: process.env.MAILGUN_DOMAIN,
+  apiKey: process.env.MAILGUN_APIKEY,
+  domain: process.env.MAILGUN_DOMAIN,
 });
 
 exports.user_register = (req, res) => {
-    if (!req.body.email || !req.body.name || !req.body.password) {
-        return res.status(500).json({
-            message: "Inputs are empty"
-        })
-    }
-    User.findOne({ email: req.body.email }).exec().then(info => {
-        if (info) {
-            return res.status(200).json({
-                message: "Email already exist!"
+  if (!req.body.email || !req.body.name || !req.body.password) {
+    return res.status(500).json({
+      message: "Inputs are empty",
+    });
+  }
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((info) => {
+      if (info) {
+        return res.status(200).json({
+          message: "Email already exist!",
+        });
+      } else {
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          name: req.body.name,
+          account_status: req.body.account_status,
+          email: req.body.email,
+          password: req.body.password,
+          is_admin: req.body.is_admin,
+        });
+        bcrypt.hash(user.password, 10, function (err, hash) {
+          user.password = hash;
+          user
+            .save()
+            .then(() => {
+              res.status(200).json({
+                message: "register success",
+              });
             })
-        }
-        else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                name: req.body.name,
-                account_status: req.body.account_status,
-                email: req.body.email,
-                password: req.body.password,
-                is_admin: req.body.is_admin
-            })
-            bcrypt.hash(user.password, 10, function (err, hash) {
-                user.password = hash;
-                user.save()
-                    .then(() => {
-                        res.status(200).json({
-                            message: "register success",
-                        })
-                    })
-                    .catch((err) => {
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-            })
-        }
+            .catch((err) => {
+              res.status(500).json({
+                error: err,
+              });
+            });
+        });
+      }
     })
-        .catch((err) => {
-            res.status(500).json({
-                error: err
-            })
-        })
-}
-
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.users_get_all = (req, res) => {
-    User.findOne({email:req.user.email}).select("name email account_status _id is_admin").exec()
-        .then(docs => {
-           if(docs){
-               console.log(docs)
-               res.status(200).json(docs);
-           }
-        }).catch(err => {
-            res.status(500).json({ error: err })
-        })
-}
-
+  User.findOne({ email: req.user.email })
+    .select("name email account_status _id is_admin")
+    .exec()
+    .then((docs) => {
+      if (docs) {
+        console.log(docs);
+        res.status(200).json(docs);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+};
 
 exports.user_login = (req, res) => {
-    if (!req.body.email || !req.body.password) {
-        return res.status(500).json({
-            message: "Inputs are empty"
-        })
-    }
-    User.findOne({ email: req.body.email })
-        .select("password account_status is_admin")
-        .exec()
-        .then((doc) => {
-            if (doc) {
-                bcrypt.compare(req.body.password, doc.password, function (err, result) {
-                    if (result) {
-                        const token = jwt.sign({
-                            email: req.body.email,
-                        },
-                            process.env.JWT_KEY, {
-                            expiresIn: 1200
-                        })
-                        if (doc.account_status == "Active") {
-                            res.status(200).json({
-                                token,
-                                message: "success"
-                            })
-                        }
-                        else if (doc.account_status == "Pending") {
-                            res.status(200).json({
-                                message: "Your Account has not been approved by the Administrator"
-                            })
-                        }
-                        else if (doc.account_status == "inactive") {
-                            res.status(200).json({
-                                message: "Your account has been inactive"
-                            })
-                        }
-                    }
-                    else {
-                        res.status(401).json({
-                            message: "invalid credentials"
-                        })
-                    }
-                    if (err) {
-                        res.status(401).json({
-                            message: "Authentication failed"
-                        })
-                    }
-                });
+  if (!req.body.email || !req.body.password) {
+    return res.status(500).json({
+      message: "Inputs are empty",
+    });
+  }
+  User.findOne({ email: req.body.email })
+    .select("password account_status is_admin")
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        bcrypt.compare(req.body.password, doc.password, function (err, result) {
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: req.body.email,
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: 1200,
+              }
+            );
+            if (doc.account_status == "Active") {
+              res.status(200).json({
+                token,
+                message: "success",
+              });
+            } else if (doc.account_status == "Pending") {
+              res.status(200).json({
+                message:
+                  "Your Account has not been approved by the Administrator",
+              });
+            } else if (doc.account_status == "inactive") {
+              res.status(200).json({
+                message: "Your account has been inactive",
+              });
             }
-            else {
-                res.status(401).json({
-                    message: "email not found "
-                })
-            }
-
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({ error: err });
+          } else {
+            res.status(401).json({
+              message: "invalid credentials",
+            });
+          }
+          if (err) {
+            res.status(401).json({
+              message: "Authentication failed",
+            });
+          }
         });
-}
-
+      } else {
+        res.status(401).json({
+          message: "email not found ",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+};
 
 exports.forgot_password = (req, res) => {
-    if (!req.body.email) {
-        return res.status(401).json({
-            message: "inputs are empty"
-        })
-    }
-    User.findOne({ email: req.body.email })
-        .exec()
-        .then((result) => {
-            if (result) {
-                const token = jwt.sign({
-                    email: req.body.email,
-                },
-                    process.env.JWT_RESET, {
-                    expiresIn: 900
-                })
-                const data = {
-                    from: "Mailgun Sandbox <noreply@optimum.com>",
-                    to: req.body.email,
-                    subject: "Reset Password",
-                    html: `There was recently a request to change the password on your account.
+  if (!req.body.email) {
+    return res.status(401).json({
+      message: "inputs are empty",
+    });
+  }
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((result) => {
+      if (result) {
+        const token = jwt.sign(
+          {
+            email: req.body.email,
+          },
+          process.env.JWT_RESET,
+          {
+            expiresIn: 900,
+          }
+        );
+        const data = {
+          from: "Mailgun Sandbox <noreply@optimum.com>",
+          to: req.body.email,
+          subject: "Reset Password",
+          html: `There was recently a request to change the password on your account.
                     <p>
                     Hello there,
                     <p>
@@ -169,125 +173,120 @@ exports.forgot_password = (req, res) => {
                          <br>
                          Optimum Solution
                          </p>
-                    </p>`
-                };
-                mg.messages().send(data, function (error, body) {
-                 
-                    if (error) {
-                        console.log(error)
-                        return res.status(401).json({
-                            error: error
-                        })
-                    }
-                    else{
-                        console.log(body.message)
-                        User.updateOne(
-                            { email: req.body.email },
-                            {
-                                $set: {
-                                    resetPasswordToken: token
-                                }
-                            },
-                        )
-                            .exec()
-                        res.status(200).json({
-                            token,
-                            message: "Email has been sent"
-                        })
-                    }
-                });
-              
-            }
-            else {
-                res.status(200).json({
-                    message: "Email not found"
-                })
-            }
-        })
-        .catch((err) => {
-            console.log(err)
-            res.status(500).json({
-                error: err
-            })
-        })
-}
+                    </p>`,
+        };
+        mg.messages().send(data, function (error, body) {
+          if (error) {
+            console.log(error);
+            return res.status(401).json({
+              error: error,
+            });
+          } else {
+            console.log(body.message);
+            User.updateOne(
+              { email: req.body.email },
+              {
+                $set: {
+                  resetPasswordToken: token,
+                },
+              }
+            ).exec();
+            res.status(200).json({
+              token,
+              message: "Email has been sent",
+            });
+          }
+        });
+      } else {
+        res.status(200).json({
+          message: "Email not found",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 exports.resetPassword = (req, res) => {
-    if (!req.body.password) {
-        return res.status(500).json({
-            message: "Input is empty"
-        })
-    }
-    User.findOne({ resetPasswordToken: req.params.token })
-        .select("email password")
-        .exec()
-        .then((result) => {
-            if (result) {
-                bcrypt.compare(req.body.password, result.password, function (err, match) {
-                    if (match) {
-                        res.status(200).json({
-                            message: "Password cannot be the same"
-                        })
+  if (!req.body.password) {
+    return res.status(500).json({
+      message: "Input is empty",
+    });
+  }
+  User.findOne({ resetPasswordToken: req.params.token })
+    .select("email password")
+    .exec()
+    .then((result) => {
+      if (result) {
+        bcrypt.compare(req.body.password, result.password, function (
+          err,
+          match
+        ) {
+          if (match) {
+            res.status(200).json({
+              message: "Password cannot be the same",
+            });
+          }
+          if (err) {
+            res.status(401).json({
+              err: err,
+            });
+          }
+          if (!match) {
+            bcrypt.hash(req.body.password, 10, function (err, hash) {
+              User.updateOne(
+                { email: result.email },
+                {
+                  $set: {
+                    password: hash,
+                    resetPasswordToken: null,
+                  },
+                }
+              )
+                .exec()
+                .then((doc) => {
+                  const data = {
+                    from: "Mailgun Sandbox <noreply@optimum.com>",
+                    to: result.email,
+                    subject: "Password has been reset",
+                    text: `Your password has been successfully reset`,
+                  };
+                  mg.messages().send(data, function (error, body) {
+                    if (error) {
+                      res.status(500).json({
+                        error: error,
+                      });
                     }
-                    if (err) {
-                        res.status(401).json({
-                            err: err
-                        })
-                    }
-                    if (!match) {
-                        bcrypt.hash(req.body.password, 10, function (err, hash) {
-                            User.updateOne(
-                                { email: result.email },
-                                {
-                                    $set: {
-                                        password: hash,
-                                        resetPasswordToken: null
-
-                                    }
-                                },
-                            )
-                                .exec()
-                                .then((doc) => {
-                                    const data = {
-                                        from: "Mailgun Sandbox <noreply@optimum.com>",
-                                        to: result.email,
-                                        subject: "Password has been reset",
-                                        text: `Your password has been successfully reset`
-                                    };
-                                    mg.messages().send(data, function (error, body) {
-                                        if (error) {
-                                            res.status(500).json({
-                                                error: error
-                                            })
-                                        }
-                                        console.log(body.message);
-                                    });
-                                    res.status(200).json({
-                                        message: "Successfully update"
-                                    })
-                                })
-                        });
-                    }
+                    console.log(body.message);
+                  });
+                  res.status(200).json({
+                    message: "Successfully update",
+                  });
                 });
-            }
-            else {
-                res.status(404).json({
-                    message: "404 not found"
-                })
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({ error: err });
+            });
+          }
         });
-}
+      } else {
+        res.status(404).json({
+          message: "404 not found",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+};
 
-
-exports.resetAuth = (req,res) => {
-    User.findOne(req.param.token)
+exports.resetAuth = (req, res) => {
+  User.findOne(req.param.token)
     .exec()
     .then((res) => {
-        if(res){
-            console.log(res)
-        }
-    })
-}
+      if (res) {
+        console.log(res);
+      }
+    });
+};
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
-
 //import express from "express"
 
 //express is basically a framework/server
-const express = require("express")
-const bodyparser = require('body-parser');
-const mongoose = require("mongoose")
-const userRoutes = require("./routes/userRoutes")
-const customerRoutes = require("./routes/customerRoutes")
-const cors = require("cors")
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 7001;
@@ -17,32 +15,26 @@ mongoose.Promise = global.Promise; // gets a response from DB fail or success
 //"mongodb+srv://optimum-leads:12345@optimumbatch7-1rg4n.mongodb.net/bankappphase2?retryWrites=true&w=majority"
 //connect to the schema
 //mongodb://localhost/bankAppPhase2
-mongoose.connect("mongodb+srv://optimum-leads:12345@optimumbatch7-1rg4n.mongodb.net/bankappphase2?retryWrites=true&w=majority", {
+mongoose.connect(
+  "mongodb+srv://optimum-leads:12345@optimumbatch7-1rg4n.mongodb.net/bankappphase2?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(cors());
 
 //body parser transposes request into language DB can understand
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 //routes
 app.use("/users", userRoutes);
-app.use("/customer", customerRoutes);
-//either deploy on server or local port
 
-app.get("/", (req, res) => { res.send(`Application is running on ${PORT}`) })
+app.get("/", (req, res) => {
+  res.send(`Application is running on ${PORT}`);
+});
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 module.exports = app;
-
-
-
-
-
-
-
-
-
