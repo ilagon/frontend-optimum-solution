4154d3612e5bd8cab9c6c2b1f3628982b05807b6
Tan Kok Keong <tan.kokkeong@optimum.net>
2020-06-24 22:05:14 +0800

    Updatd submit transfer - KK

--- frontend/src/components/customer/TransferMoney/SubmitTransferPage.js
+++ frontend/src/components/customer/TransferMoney/SubmitTransferPage.js
@@ -67,13 +67,13 @@ export default function SubmitTransferPage() {
     payment_type: "Transfer",
     payment_amount: states.transferAmount,
     transfer_number: 1,
-    creditcardId: states.senderCreditCardId,
+    creditcardId: states.senderCreditCardID,
   })
   .then((response) => {
     console.log(response);
     axios
       .patch("http://localhost:9002/creditcards/updateBalance", {
-        creditcard_Id: states.senderCreditCardId,
+        creditcard_Id: states.senderCreditCardID,
         creditcard_balance:
         states.senderCreditCardBalance -
           states.transferAmount,
