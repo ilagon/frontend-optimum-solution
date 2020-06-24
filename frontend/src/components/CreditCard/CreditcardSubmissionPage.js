a76e561cdfe0090d605047a6620824e6dec679ef
Zafy <zafirahrosley96@gmail.com>
2020-06-24 10:38:47 +0800

    changes for apply creditcard pages

--- frontend/src/components/CreditCard/CreditcardSubmissionPage.js
+++ frontend/src/components/CreditCard/CreditcardSubmissionPage.js
@@ -1,91 +1,63 @@
-import React from 'react'; 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { withStyles, makeStyles,  } from '@material-ui/core/styles';
-import { List } from 'semantic-ui-react';
-import styles from "./css/ApplyCreditcard.module.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "../Overview/Overview.css";
+import {
+  BrowserRouter as Router,
+  Link,
+} from "react-router-dom";
 
-const useStyles = makeStyles((theme)=>({
-  root: {
-    width: '80%',
-  },
-  container: {
-    maxHeight: 640,
-  },
-  '& > *': {
-    margin: theme.spacing(1),
-    width: '25ch',
-  },
-  
-  
+const useStyles = makeStyles((theme) => ({
+  appBarSpacer: theme.mixins.toolbar,
 }));
 
+export default function ApplyCreditcardSubmit() {
+  const classes = useStyles();
+  const referenceNum = "1234567";
+  const creditcardName = "Hannah";
+  const email = "hannah@gmail.com";
 
-class Apply_CC_Confirm extends React.Component {
+  const handleChange = (e) => {
+  };
 
-  saveAndContinue = (e) => {
-    e.preventDefault();
-    this.props.nextStep();
-}
 
-back  = (e) => {
-    e.preventDefault();
-    this.props.prevStep();
-}
-
-  
-  render() {
-    
     return (
-      <div >
-        <header>
-          Credit Card Application
-        </header>
-      
-          <form action='#' >
-            
-          <div>
-                <List>
-                    <List.Item>
-                        <List.Content>Reference No.:12345 </List.Content>
-                    </List.Item>
-
-                    <List.Item>
-                        <List.Content>Hannah</List.Content>
-                    </List.Item>
-                    
-                    <List.Item>
-                        <List.Content>123 pasir ris</List.Content>
-                    </List.Item>
-
-                    <List.Item>
-                        <List.Content>
-                            <a href='mailto:jack@semantic-ui.com'>hannah@gmail.com</a>
-                        </List.Content>
-                    </List.Item>
-
-                    <List.Item>
-                        <List.Content>91234567</List.Content>
-                    </List.Item>
-                </List>
-
-        </div>
-            <div >
-              <Button type='submit' className={styles.btnApply}>
-                  Apply   
-                </Button >
-            <br></br>
-                 <Button type='submit' className={styles.btnCancel} >
+      <React.Fragment>
+        <main className="content">
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="md" className="container">
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <h2 className="darkRedColor">Credit Card Application</h2>
+                <p className="darkBlueColor">Ref No.: {referenceNum}</p>
+                <p className="darkBlueColor">{creditcardName}</p>
+                <p className="darkBlueColor">{email}</p>
+              </Grid>
+            </Grid>
+            <Link to="/apply-creditcard/creditcard-confirm">
+              <Button
+                variant="contained"
+                id="cardApplyBtn"
+                onClick={handleChange}
+              >
+                Apply
+              </Button>
+            </Link>
+            <Link to="/">
+              <Button
+                variant="contained"
+                id="cardCancelBtn"
+                onClick={handleChange}
+              >
                 Cancel
-                </Button >
-            
-            </div>
-            </form>
-      
-      </div>
+              </Button>
+            </Link>
+          </Container>
+        </main>
+      </React.Fragment>
     );
   }
-}
-
-export default Apply_CC_Confirm;
-// pushing again
