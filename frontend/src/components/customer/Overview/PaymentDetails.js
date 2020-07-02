7dc34cf89b67e9e5b2d06e2f7548bc93f519d5f4
Ain <RoquetteAce@users.noreply.github.com>
2020-07-02 15:42:24 +0800

    OMG FRONTEND WORKS

--- frontend/src/components/customer/Overview/PaymentDetails.js
+++ frontend/src/components/customer/Overview/PaymentDetails.js
@@ -1,40 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { makeStyles } from '@material-ui/core/styles';
-import TableContainer from '@material-ui/core/TableContainer';
-import TablePagination from '@material-ui/core/TablePagination';
-import './Overview.css';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+import TableContainer from "@material-ui/core/TableContainer";
+import TablePagination from "@material-ui/core/TablePagination";
+import "./Overview.css";
 
 const columns = [
-  { id: 'transfer_number', label: 'Payment Id'},
-  { id: 'payment_type', label: 'Payment Type'},
+  { id: "transfer_number", label: "Payment Id" },
+  { id: "payment_type", label: "Payment Type" },
   {
-    id: 'payment_amount',
-    label: 'Payment Amount',
-    format: (value) => "$" + value.toFixed(2)
+    id: "payment_amount",
+    label: "Payment Amount",
+    format: (value) => "$" + value.toFixed(2),
   },
   {
-    id: 'date_time',
-    label: 'Date & Time',
+    id: "date_time",
+    label: "Date & Time",
   },
   {
-    id: 'customer_id',
-    label: 'Customer ID',
+    id: "customer_id",
+    label: "Customer ID",
   },
   {
-    id: 'transfer_number',
-    label: 'Transfer ID',
+    id: "transfer_number",
+    label: "Transfer ID",
   },
 ];
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
+    width: "100%",
   },
   container: {
     maxHeight: 440,
@@ -49,11 +49,14 @@ export default function PaymentDetails(props) {
   const creditCardId = props.creditCardId;
   const customerId = props.customerId;
 
-    axios.get("http://localhost:9000/payment_history/" + creditCardId)
-    .then(res => {
+  axios
+    .get("http://localhost:9000/payment_history/checkPayment", {
+      creditcardId: creditCardId,
+    })
+    .then((res) => {
       setRows([...res.data.Users]);
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
   console.log(rows);
 
   const handleChangePage = (event, newPage) => {
@@ -67,7 +70,7 @@ export default function PaymentDetails(props) {
 
   return (
     <React.Fragment>
-      <div >
+      <div>
         <h2 className="paymentHistoryTitle">Payment History</h2>
       </div>
       <TableContainer className={classes.container}>
@@ -86,23 +89,30 @@ export default function PaymentDetails(props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align} className="paymentTableCell">
-                        {
-                          column.id === 'customer_id' ? customerId :
-                          column.format && typeof value === 'number' ? column.format(value) : value
-                        }
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          className="paymentTableCell"
+                        >
+                          {column.id === "customer_id"
+                            ? customerId
+                            : column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
